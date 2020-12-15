94cce0276bc1b3ebeb9a42025dfd8e4a58085ffd
Jaden-Reklaw <jwalker117@gmail.com>
2020-12-15 11:57:40 -0600

    added datepicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,56 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//My Components
+import { ExpandingPanelComponent } from './expanding-panel/expanding-panel.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { NavComponent } from './nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+
 //Import material Modules
 import{ MatInputModule } from '@angular/material/input';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { ExpandingPanelComponent } from './expanding-panel/expanding-panel.component';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+
+// import {
+//   MatInputModule,
+//   MatToolbarModule,
+//   MatButtonModule,
+//   MatSidenavModule,
+//   MatIconModule,
+//   MatListModule,
+//   MatGridListModule,
+//   MatCardModule,
+//   MatMenuModule,
+//   MatTableModule,
+//   MatPaginatorModule,
+//   MatSortModule,
+//   MatExpansionModule,
+//   MatDatepickerModule,
+//   MatNativeDateModule
+// } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     DashboardComponent,
-    ExpandingPanelComponent
+    ExpandingPanelComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
@@ -49,6 +74,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatPaginatorModule,
     MatSortModule,
     MatExpansionModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
